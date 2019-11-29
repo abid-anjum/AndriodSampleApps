3992fe7a2cc8d19b4a467649b5c797845e3c2e3e
abidhussain <abid786@gmail.com>
2019-11-30 02:32:35 +0300

    add loginregistrationapp

---
+++ LoginRegistration/app/src/main/java/com/androstock/loginregistration/RegistrationActivity.java
@@ -0,0 +1,31 @@
+package com.androstock.loginregistration;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/*
+    Created By
+    Ferdousur Rahman Sarker
+    www.androstock.com
+*/
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        TextView signIn_text = findViewById(R.id.signIn_text);
+        signIn_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }
+}
